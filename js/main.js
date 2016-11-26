000683534b1a3a3fc602723a82c0ded62fd6ddbe
ilona <ilona.milz@gmail.com>
2016-11-26 15:24:33 +0200

    test

---
+++ js/main.js
@@ -0,0 +1,46 @@
+let cars=['bmw', 'mazda', 'opel'];
+let speed=300;
+
+console.log(' все мои '+cars+' ездят на корости '+speed);
+
+console.log(`все мои ${cars} ездят на скорости ${speed}`);
+
+
+let myName = 'ilona';
+
+console.log(myName[0].toUpperCase() + myName.slice(1));
+
+let newName = 'iLONA'
+
+console.log(newName[0].toUpperCase() + newName.slice(1).toLowerCase());
+
+//
+
+let twoString = '2';
+let twentyString = '20';
+
+console.log(Number(twentyString)+twoString);
+console.log(twoString==Number(twoString));
+
+console.log(twoString===Number(twoString));
+
+let currentYear = '2015';
+// вывести  конcоле год без тысячи
+
+console.log(+currentYear % 2000);
+
+console.log(![] == false);
+console.log(Math.max(1,2,5,25, 'hello'));
+
+//
+
+let userName = 'Ilona';
+console.log(typeof userName);
+
+var myobj = {name:Oleg};
+obj[age]= 27;
+
+
+
+
+
